2e6f6a385a765726abaf360f749d5ded202ecb84
Azman Abdullah <azmanabdlh@yahoo.com>
2022-04-20 23:13:21 +0900

    menambahkan sidebar component

---
+++ components/mengenal-kami/sidebar.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+
+
+const SidebarCollapse = styled.aside`
+  flex: 1;
+  border: 1px solid #ddd;
+  margin-right: 1.2rem;
+  border-radius: 3px;
+  position: sticky;
+  top: 20px;
+  display: inline-block;
+  height: max-content;
+
+`;
+
+const SidebarTitle = styled.h2`
+  text-align: center;
+  font-family: 'Plus Jakarta Sans', sans-serif;
+  font-size: .9rem;
+
+  svg {
+    display: inline-block;
+    margin-right: .8rem;
+  }
+`;
+
+const SidebarContent = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`;
+
+const SidebarItem = styled.li`
+  font-family: 'Plus Jakarta Sans', sans-serif;
+  font-size: .9rem;
+  border-bottom: 1px solid #ddd;
+
+  &:last-child {
+    border-bottom: 0;
+  }
+
+  > a {
+    text-decoration: none;
+    padding: 1rem;
+    display: block;
+
+    ${
+      props => props.active && `
+        color: var(--primary);
+        font-weight: 700;
+      `
+    }
+  }
+`;
+
+
+export default function Sidebar() {
+
+  const router = useRouter();
+
+
+  return (
+    <SidebarCollapse>
+      <SidebarTitle>
+        <FontAwesomeIcon icon={faBars} />
+        Tinjauan cepat
+      </SidebarTitle>
+
+      <SidebarContent>
+        <SidebarItem active={/sejarah/.test(router.pathname)}>
+          <Link href="/" passHref>Sejarah</Link>
+        </SidebarItem>
+        <SidebarItem>
+          <Link href="/" passHref>Struktur organisasi</Link>
+        </SidebarItem>
+        <SidebarItem>
+          <Link href="/" passHref>Tugas dan fungsi</Link>
+        </SidebarItem>
+        <SidebarItem>
+          <Link href="/" passHref>Command Center</Link>
+        </SidebarItem>
+      </SidebarContent>
+    </SidebarCollapse>
+  );
+}
--- pages/mengenal-kami/sejarah.js
+++ pages/mengenal-kami/sejarah.js
@@ -1,8 +1,22 @@
 import Container from "@/components/container";
 import Breadcrumb from "@/components/breadcrumb";
 import styled from 'styled-components';
+import Sidebar from '@/components/mengenal-kami/sidebar';
+
+const FlexRow = styled.div`
+  display: flex;
+  margin: 3rem 0;
+  margin-top: 2rem;
+`;
+
+
+const Content = styled.div`
+  flex: 3.5;
+`;
+
 
 export default function History() {
+
   return (
     <Container>
       <Breadcrumb
@@ -12,6 +26,12 @@ export default function History() {
         ]}
       />
 
+      <FlexRow>
+        <Sidebar/>
+        <Content>
+          content
+        </Content>
+      </FlexRow>
     </Container>
   );
 }
