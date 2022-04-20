e8be26c8b61d4aefc8b61eceb9aefe1afcc4831f
Azman Abdullah <azmanabdlh@yahoo.com>
2022-04-20 18:21:23 +0900

    menambahkan informasi kondisi cuaca

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import Divider from '@/components/divider';
 import Post from '@/components/post';
 import Container from '@/components/container';
 import useWeather from '@/utils/weather';
+import weatherConditionId from '@/lang/weather-condition-id';
 
 
 const title = 'Pemerintah Kota Ambon &#8211; Laman resmi Dinas komunikasi, informatika dan persandian kota Ambon';
@@ -32,6 +33,25 @@ const WeatherText = styled.p`
   margin: 0;
   margin-left: .5rem;
   margin-top: .6rem;
+
+  > span {
+    position: relative;
+    display: inline-block;
+    color: var(--primary);
+    margin-right: .3rem;
+    &::after {
+      position: absolute;
+      content: '';
+      height: 2px;
+      width: 2px;
+      border: 1px solid var(--primary);
+      border-radius: 50%;
+      display: inline-block;
+      top: 2px;
+      right: -0.3125rem;
+
+    }
+  }
 `;
 
 
@@ -67,7 +87,10 @@ export default function Home() {
                 layout="fixed"
               />
               <WeatherText>
-                Kondisi cuaca {location.name}, {location.region} {location.country} saat ini
+                <span>{current.feelslike_c}</span>
+                C kondisi cuaca {location.name}, {location.region} {location.country} saat ini {
+                  weatherConditionId[current.condition.text]
+                }.
               </WeatherText>
             </>
           )}
